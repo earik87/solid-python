b85c806f897cfa7abe3623a33a87426ab402dc55
Enis Arik <enisarik1987@gmail.com>
2023-02-12 22:42:10 +0300

    student instance as an input for registerer class.

--- single-responsibility-good.py
+++ single-responsibility-good.py
@@ -8,11 +8,11 @@ class Student:
         print(f"Student {self.name} {self.surname} has ID: {self.identification_n}.")
 
 class Registerer:
-    def register(self, identification_n):
-        print(f"Student with ID {identification_n} is registered.")
+    def register(self, student):
+        print(f"Student with ID {student.identification_n} is registered.")
         self.registered = True
 
 Enis = Student("Enis", "Arik", 115)
 Enis.get_student_info()
 registerer = Registerer()
-registerer.register(115)
+registerer.register(Enis)
